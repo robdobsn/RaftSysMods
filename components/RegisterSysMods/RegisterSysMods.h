23848d8047178bb667aa4376aedb38295262e899
Rob Dobson <rob@dobson.com>
2025-11-15 17:57:55 +0900

    Update RegisterSysMods.h

--- components/RegisterSysMods/RegisterSysMods.h
+++ components/RegisterSysMods/RegisterSysMods.h
@@ -11,23 +11,35 @@
 
 #include "sdkconfig.h"
 #include "SysManager.h"
-#include "BLEManager.h"
-#include "CommandFile.h"
-#include "CommandSerial.h"
-#include "CommandSocket.h"
-#include "ESPOTAUpdate.h"
-#include "FileManager.h"
-#include "LogManager.h"
-#include "MQTTManager.h"
-#include "NetworkManager.h"
-#include "SampleCollectorJSON.h"
-#include "SerialConsole.h"
-#include "StatePublisher.h"
 
 // Check if networking is enabled
 #if defined(CONFIG_ESP_WIFI_ENABLED) || defined(CONFIG_ETH_USE_ESP32_EMAC) || defined(CONFIG_ETH_USE_SPI_ETHERNET) || defined(CONFIG_ETH_USE_OPENETH) || defined(CONFIG_ETH_USE_RMII_ETHERNET)
 #define NETWORKING_IS_ENABLED
 #endif
+
+// Include headers conditionally based on what's available
+#if CONFIG_BT_ENABLED
+#include "BLEManager.h"
+#endif
+#include "CommandFile.h"
+#include "CommandSerial.h"
+#ifdef NETWORKING_IS_ENABLED
+#include "CommandSocket.h"
+#endif
+#ifdef ESP_PLATFORM
+#include "ESPOTAUpdate.h"
+#include "LogManager.h"
+#endif
+#include "FileManager.h"
+#ifdef NETWORKING_IS_ENABLED
+#include "MQTTManager.h"
+#include "NetworkManager.h"
+#endif
+#ifdef ESP_PLATFORM
+#include "SampleCollectorJSON.h"
+#endif
+#include "SerialConsole.h"
+#include "StatePublisher.h"
 
 namespace RegisterSysMods
 {
@@ -43,7 +55,9 @@ namespace RegisterSysMods
         sysManager.registerSysMod("CommandFile", CommandFile::create);
 
         // Command Serial
+#ifdef ESP_PLATFORM
         sysManager.registerSysMod("CommandSerial", CommandSerial::create);
+#endif
         
         // Command Socket
 #ifdef NETWORKING_IS_ENABLED
@@ -51,13 +65,17 @@ namespace RegisterSysMods
 #endif
 
         // ESPOTAUpdate
+#ifdef ESP_PLATFORM
         sysManager.registerSysMod("ESPOTAUpdate", ESPOTAUpdate::create);
+#endif
         
         // FileManager
         sysManager.registerSysMod("FileManager", FileManager::create, true);
 
         // LogManager
+#ifdef ESP_PLATFORM
         sysManager.registerSysMod("LogManager", LogManager::create);
+#endif
 
         // MQTTManager
 #ifdef NETWORKING_IS_ENABLED
@@ -70,7 +88,9 @@ namespace RegisterSysMods
 #endif
 
         // Sample collector JSON
+#ifdef ESP_PLATFORM
         sysManager.registerSysMod("SamplesJSON", SampleCollectorJSON::create);
+#endif
 
         // Serial Console
         sysManager.registerSysMod("SerialConsole", SerialConsole::create);
